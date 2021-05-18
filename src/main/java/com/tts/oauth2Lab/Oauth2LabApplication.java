0327559f43c26b1aa8051a008d6d7dd924fb7684
katemejaski <katmackjj@gmail.com>
2021-05-18 16:22:33 -0400

    Initial commit

---
+++ src/main/java/com/tts/oauth2Lab/Oauth2LabApplication.java
@@ -0,0 +1,13 @@
+package com.tts.oauth2Lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2LabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/oauth2Lab/Oauth2LabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.oauth2Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
